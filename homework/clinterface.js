b7c3ec7ec2792acbffadba437be3a6ed539ff367
cranis <cranis2@naver.com>
2023-07-25 08:47:34 +0900

    fefefefef

---
+++ homework/clinterface.js
@@ -0,0 +1,28 @@
+const readline = require("readline");
+
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
